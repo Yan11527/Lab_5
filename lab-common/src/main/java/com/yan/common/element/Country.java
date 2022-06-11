ce6b901b18352cb37877d8d983120b82139d29fb
Yan11527 <106553407+Yan11527@users.noreply.github.com>
2022-06-11 03:33:27 +0300

    Delete lab-common directory

--- lab-common/src/main/java/com/yan/common/element/Country.java
+++
@@ -1,9 +0,0 @@
-package com.yan.common.element;
-
-public enum Country {
-    UNITED_KINGDOM,
-    CHINA,
-    THAILAND,
-    SOUTH_KOREA,
-    JAPAN
-}
--- lab-common/src/main/java/com/yan/common/element/FormOfEducation.java
+++
@@ -1,7 +0,0 @@
-package com.yan.common.element;
-
-public enum FormOfEducation {
-    DISTANCE_EDUCATION,
-    FULL_TIME_EDUCATION,
-    EVENING_CLASSES
-}
